6707156d5ddd0c413100f4aacc91557b2efad31a
iwayIrshad <iway.app7@gmail.com>
2025-10-26 03:39:39 +0530

    Inngest integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,39 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
-import LogoutButton from "./logout";
+"use client";
 
-const Page = async () => {
-  await requireAuth();
-  const data = await caller.getUsers();
+import { useTRPC } from "@/trpc/client";
+import LogoutButton from "./logout";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data, isLoading, error } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Job queued...");
+    },
+  }));
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       Protected server component
       <div>
-        {JSON.stringify(data, null, 2)}
+        {isLoading && <p>Loading...</p>}
+        {error && <p className="text-red-500">Error: {error.message}</p>}
+        {data && (
+          <div>
+            <p>Workflows ({data.length}):</p>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        )}
+        {!isLoading && !error && data?.length === 0 && (
+          <p>No workflows found. Create one!</p>
+        )}
       </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton />
     </div>
   );
