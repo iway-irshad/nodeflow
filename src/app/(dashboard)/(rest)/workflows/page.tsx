15fb0dd4060c20920d3b8e36ae805ddc2ce7f32e
iwayIrshad <iway.app7@gmail.com>
2025-11-07 00:31:08 +0530

    feat: implement workflows search and pagination, add nuqs integration

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,14 +1,21 @@
 import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+import { workflowsParamsLoader } from "@/features/workflows/server/params-loader";
 import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
 import { HydrateClient } from "@/trpc/server";
+import { SearchParams } from "nuqs/server";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-const Page = async () => {
-    await requireAuth();
+type Props = {
+    searchParam: Promise<SearchParams>;
+}
 
-    prefetchWorkflows();
+const Page = async ({ searchParam }: Props) => {
+    await requireAuth();
+    const params = await workflowsParamsLoader(searchParam);
+
+    prefetchWorkflows(params);
 
     return (
         <WorkflowsContainer>
